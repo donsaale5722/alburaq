ba34d1469e0e37e24feec035d989bd8a2c354a72
donsaale <whitedevilbca3@gmail.com>
2022-10-30 03:10:23 -0700

    title change

--- src/component/home.js
+++ src/component/home.js
@@ -8,7 +8,7 @@ const Home = () =>{
     <div className="container-fluid mb-3">
         <div className="row px-xl-5">
             <div className="col-lg-8">
-            
+
                 <div id="header-carousel" className="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
                     <ol className="carousel-indicators">
                         <li data-target="#header-carousel" data-slide-to="0" className="active"></li>
@@ -20,7 +20,7 @@ const Home = () =>{
                             <img className="position-absolute w-100 h-100" src="img/oud-mikhlaat.jfif" alt="description"  style={{objectFit: "cover"}}/>
                             <div className="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                 <div className="p-3" style={{maxWidth: "700px"}}>
-                                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men's Perfumes</h1>
+                                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men'ss Perfumes</h1>
                                     <p className="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
                                     <NavLink className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" to="#">Shop Now</NavLink>
                                 </div>
