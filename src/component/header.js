1ea719dce92d7e0ee283f371291fc03f2af5a2b2
donsaale <whitedevilbca3@gmail.com>
2022-11-04 04:00:45 -0700

    update nav bar

--- src/component/header.js
+++ src/component/header.js
@@ -12,7 +12,7 @@ const Header = () =>{
         </div>
         <div className="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
             <div className="col-lg-4">
-                <NavLink to="" className="text-decoration-none">
+                <NavLink to="" className="mobile text-decoration-none">
                     <span className="h1 text-uppercase text-primary bg-dark px-2">Al</span> 
                     <span className="h1 text-uppercase text-dark bg-primary px-2 ml-n1">BuraQ</span>
                 </NavLink>
@@ -74,15 +74,15 @@ const Header = () =>{
                         <span className="h1 text-uppercase text-dark bg-light px-2">Al</span>
                         <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">BuraQ</span>
                     </NavLink>
-                    <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    {/* <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                         <div className="navbar-nav mr-auto py-0">
                             <NavLink to="/" className="nav-item nav-link active">Home</NavLink>
-                            <NavLink to="/product" className="nav-item nav-link">Product</NavLink>
+                            <NavLink to="/product" className="nav-item nav-link">Product</NavLink> */}
                             {/* <NavLink to="detail.html" className="nav-item nav-link">Shop Detail</NavLink> */}
-                            <div className="nav-item dropdown">
+                            {/* <div className="nav-item dropdown">
                                 <NavLink to="#" className="nav-link dropdown-toggle" data-toggle="dropdown">Pages <i className="fa fa-angle-down mt-1"></i></NavLink>
                                 <div className="dropdown-menu bg-primary rounded-0 border-0 m-0">
                                     <NavLink to="/cart" className="dropdown-item">Shopping Cart</NavLink>
@@ -90,7 +90,7 @@ const Header = () =>{
                                 </div>
                             </div>
                             <NavLink to="/contact" className="nav-item nav-link">Contact</NavLink>
-                        </div>
+                        </div> */}
                         <div className="navbar-nav ml-auto py-0 d-none d-lg-block">
                             <NavLink to="" className="btn px-0" style={{paddingBottom: "2px",marginLeft:"5px", border:"hidden"}}>
                                 <i className="fas fa-heart text-primary"></i>
@@ -101,12 +101,40 @@ const Header = () =>{
                                 <span className="badge text-secondary border border-secondary rounded-circle" style={{paddingBottom: "2px",marginLeft:"5px", border:"hidden"}}>0</span>
                             </NavLink>
                         </div>
-                    </div>
+                    {/* </div> */}
                 </nav>
             </div>
         </div>
     </div>
     {/* <!-- Navbar End --> */}
+    <div class="wrapper">
+         <input type="checkbox" id="btn" hidden/>
+         <label for="btn" class="menu-btn">
+         <i class="fas fa-bars"></i>
+         <i class="fas fa-times"></i>
+         </label>
+         <nav id="sidebar">
+            <div class="title">
+               Side Menu
+            </div>
+            <ul class="list-items">
+               <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+               <li><a href="#"><i class="fas fa-sliders-h"></i>Clients</a></li>
+               <li><a href="#"><i class="fas fa-address-book"></i>Services</a></li>
+               <li><a href="#"><i class="fas fa-cog"></i>Settings</a></li>
+               <li><a href="#"><i class="fas fa-stream"></i>Features</a></li>
+               <li><a href="#"><i class="fas fa-user"></i>About us</a></li>
+               <li><a href="#"><i class="fas fa-globe-asia"></i>Languages</a></li>
+               <li><a href="#"><i class="fas fa-envelope"></i>Contact us</a></li>
+               <div class="icons">
+                  <a href="#"><i class="fab fa-facebook-f"></i></a>
+                  <a href="#"><i class="fab fa-twitter"></i></a>
+                  <a href="#"><i class="fab fa-github"></i></a>
+                  <a href="#"><i class="fab fa-youtube"></i></a>
+               </div>
+            </ul>
+         </nav>
+      </div>
         </>
     );
 }
